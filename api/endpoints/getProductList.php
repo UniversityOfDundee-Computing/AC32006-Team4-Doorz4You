f86f7ad3921e0eaf94ffa19508a331e6861a556e
Adam Mathieson <adam@genav.ch>
2022-11-28 16:07:46 +0000

    + price

--- api/endpoints/getProductList.php
+++ api/endpoints/getProductList.php
@@ -1,6 +1,6 @@
 <?php
 function getProductListHandler($pdo) {
-    $stmnt = $pdo->prepare("SELECT JobType, JobName, Description FROM jobtype");
+    $stmnt = $pdo->prepare("SELECT JobType, JobName, Description, Price FROM jobtype");
     $stmnt->execute([]);
     return json_encode($stmnt->fetchAll(), JSON_PRETTY_PRINT);
 }
