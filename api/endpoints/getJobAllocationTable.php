7228b1c484fec89c316e41f0bc94b7098124cbe8
Adam Mathieson <adam@genav.ch>
2022-11-30 11:05:07 +0000

    + probs works better now

--- api/endpoints/getJobAllocationTable.php
+++ api/endpoints/getJobAllocationTable.php
@@ -2,6 +2,6 @@
 function getJobAllocationTableHandler(PDO $pdo) {
     $staffDetails = getStaffDetail($pdo);
     $stmnt = $pdo->prepare("SELECT * FROM joballocation where Location = ?");
-    $stmnt->execute([$staffDetails[0]['Location']]);
+    $stmnt->execute([$staffDetails[0]['location']]);
     return json_encode($stmnt->fetchAll(), JSON_PRETTY_PRINT);
 }
