4d419f57dfb4225a0ebc9eb1abbfd48d808a9175
Adam Mathieson <adam@genav.ch>
2022-11-30 11:24:44 +0000

    + Slightly better resp codes

--- api/index.php
+++ api/index.php
@@ -1,7 +1,8 @@
 <?php
 include_once "inc.php";
-
+http_response_code(404);
 if (isset($_GET['getProductList'])) {
+    http_response_code(200);
     $pdo = PDO_config("22ac3u04", "abc322");
     include_once "endpoints/getProductList.php";
     header("content-type: application/json");
@@ -9,6 +10,7 @@ if (isset($_GET['getProductList'])) {
 }
 
 if (isset($_GET['createOrder'])) {
+    http_response_code(200);
     $pdo = PDO_config("22ac3u04", "abc322");
     include_once "endpoints/createOrder.php";
     header("content-type: text/txt");
@@ -16,12 +18,14 @@ if (isset($_GET['createOrder'])) {
 }
 
 if (isset($_GET['getJobAllocationTable'])) {
+    http_response_code(200);
     $pdo = PDO_config("22ac3u04", "abc322");
     include_once "endpoints/getJobAllocationTable.php";
     header("content-type: application/json");
     echo getJobAllocationTableHandler($pdo);
 }
 if (isset($_GET['userConnect'])) {
+    http_response_code(200);
     $pdo = PDO_config("22ac3u04", "abc322");
     include_once "endpoints/userConnect.php";
     header("content-type: application/json");
