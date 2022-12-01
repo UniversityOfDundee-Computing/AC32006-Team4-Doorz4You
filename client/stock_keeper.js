05589002dbe9320d44e40149ce206d3f0070e069
Keir Cameron <keircameron22@gmail.com>
2022-12-01 01:34:53 +0000

    Made StockKeeper page let me know any changes

---
+++ client/stock_keeper.js
@@ -0,0 +1,63 @@
+new Vue({
+    el: '#app',
+    data: {
+         stockList: [{
+            part: 'Door Handle',
+            PartIdNumber: '12345',
+            amount: '25',
+            location: 'A7',
+            reserved: '0',
+            actions: ''
+        },
+        ],
+
+        stockPreparationTeam1: [{
+          required: 'Silver Door Handle',
+          quantity: '1',
+          collectionStatus: 1
+       },{
+        required: 'Screws',
+        quantity: '10',
+        collectionStatus: 0
+       },{
+        required: 'Blue Paint Tub',
+        quantity: '1',
+        collectionStatus: 1
+       },{
+        required: 'Hinges',
+        quantity: '4',
+        collectionStatus: 0
+       },{
+        required: 'Sealant',
+        quantity: '3',
+        collectionStatus: 1
+       },
+      ],
+
+      stockPreparationTeam2: [{
+        required: 'Wood Glue',
+        quantity: '1',
+        collectionStatus: 0
+    },
+    ],
+
+    },
+  
+    created: function() {
+    },
+  
+    methods: {
+      testMethod: function() {
+        console.log('yes');
+      },
+
+      changeTeamAllocation: function(stock) {
+        console.log(stock);
+      },
+
+      changeJobStatus: function(stock) {
+        console.log(stock);
+      }
+    }
+  });
+  
