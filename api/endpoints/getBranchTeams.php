58e51362db05be0d2a77f764ce7cd7acf8bb13f0
Adam Mathieson <adam@genav.ch>
2022-12-05 16:00:02 +0100

    + Should correct Zvony's issue

--- api/endpoints/getBranchTeams.php
+++ api/endpoints/getBranchTeams.php
@@ -9,5 +9,21 @@ function getBranchTeamsHandler(PDO $pdo) {
     }
     $stmnt = $pdo->prepare("SELECT * FROM teamslist where Location = ?");
     $stmnt->execute([$staffDetails[0]['location']]);
-    return json_encode($stmnt->fetchAll(), JSON_PRETTY_PRINT);
+    $teams = [];
+    $staffStmnt = $pdo->prepare("SELECT * FROM teamemployeelist where TeamID = ?");
+
+    foreach ($stmnt->fetchAll() as $team) {
+        $staffStmnt->execute([$team['TeamID']]);
+        $teams[] = [
+            "name"=>$team['DisplayName'],
+            "vehicle"=>[
+                "id"=>$team['Vehicle'],
+                "brand"=>$team['Brand'],
+                "model"=>$team['Model'],
+            ],
+            "id"=>$team['TeamID'],
+            "staff"=>$staffStmnt->fetchAll()
+        ];
+    }
+    return json_encode($teams, JSON_PRETTY_PRINT);
 }
