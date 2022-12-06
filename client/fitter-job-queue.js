95ec6b892d4bf0727af20ecc77afb4a0e0b7397c
Zvonimir Delas <zvonimird@dump.hr>
2022-12-06 10:40:55 +0000

    various

--- client/fitter-job-queue.js
+++ client/fitter-job-queue.js
@@ -1,50 +1,56 @@
-new Vue({
+var vue = new Vue({
     el: '#app',
     data: {
-        jobsList: [{
-           
-            jobText: 'Handle replacement',
-            address : 'DD2 3NQ',
-            customerComment : '',
-            jobStatus: 1
-        },{
-            
-            jobText: 'Hinge replacement',
-            address : 'DD1 5DL',
-            customerComment : '',
-            jobStatus: 1
-        },{
-            
-            jobText: 'Varnishing',
-            address : 'DD6 4DL',
-            customerComment : 'Make it slick',
-            jobStatus: 1
-        },{
-            
-            jobText: 'Door Re-Painting',
-            address : 'DD1 5DL',
-            customerComment : 'Would like green paint',
-            jobStatus: 0
-        },
-        {
-            
-            jobText: 'Door Re-Sealing',
-            address : 'DD4 6EP',
-            customerComment : '',
-            jobStatus: 0
-        },{
-            
-            jobText: 'Door Re-Painting',
-            address : 'DD5 2GP',
-            customerComment : 'Red paint',
-            jobStatus: 2
-        },
-        ],
+        // jobsList: [
+        //     {
+        //         "CustomerNo": "CUST_63861ca3515366.28040455",
+        //         "PostCode": "DD1 1PJ",
+        //         "TeamName": "Team 01",
+        //         "JobNo": 2,
+        //         "Status": "OPEN",
+        //         "StartDate": "0000-00-00 00:00:00",
+        //         "EndDate": null,
+        //         "JobType": "1",
+        //         "JobName": "Door Handle Replacement",
+        //         "AllocatedTeam": "01",
+        //         "Location": "01"
+        //     },
+        //     {
+        //         "CustomerNo": "cust_48392847586958473829222",
+        //         "PostCode": "DD21RQ",
+        //         "TeamName": "Team 01",
+        //         "JobNo": 4,
+        //         "Status": "ALLOCATED",
+        //         "StartDate": "2017-07-23 13:10:11",
+        //         "EndDate": null,
+        //         "JobType": "2",
+        //         "JobName": "Door Hinge Replacement",
+        //         "AllocatedTeam": "01",
+        //         "Location": "01"
+        //     }
+        // ],
+
+        jobsList: []
 
     },
   
     created: function() {
         this.loggedInUsername = localStorage.getItem('firstname');
+        let localToken = localStorage.getItem('token');
+        var vm = this;
+
+        axios({
+            method: "get",
+            url: `${apiUrl}?getTeamJobs`,
+            headers: {
+                token: localToken,
+            },
+        }).then((response) => {
+            console.log(response.data);
+            console.log(Object.values(response.data)[0]);
+            vm.jobsList = Object.values(response.data)[0].jobs;
+            // console.log(response.data[0]);
+        });
     },
   
     methods: {
