e86371a99a1b530b66783945d2df9858dcdeb05f
Adam Mathieson <adam@genav.ch>
2022-12-06 09:19:14 +0000

    + should automatically get a team number

--- api/endpoints/createTeam.php
+++ api/endpoints/createTeam.php
@@ -7,8 +7,15 @@ function createTeamHandler(PDO $pdo) {
             "error"=>$exception->getMessage()
         ], JSON_PRETTY_PRINT);
     }
+    $stmnt = $pdo->prepare("SELECT count(TeamID) as count FROM team where Location = ? group by Location");
+    $stmnt->execute([$staffDetails[0]['location']]);
+    $displayName = "#0";
+    if ($stmnt->rowCount() > 0)
+        $displayName = "#" . $stmnt->fetchAll()[0]['count'];
+    if (isset($_POST['displayName']))
+        $displayName = $_POST['displayName'];
     $stmnt = $pdo->prepare("INSERT INTO team values (TeamID = ?, Vehicle = '', Location = ?, DisplayName = ?)");
-    $stmnt->execute([uniqid("team_", true), $staffDetails[0]['location'], $_POST['displayName']]);
+    $stmnt->execute([uniqid("team_", true), $staffDetails[0]['location'], $displayName]);
 
     return json_encode([
         "status"=>"ok"
